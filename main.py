821d57ecb0a5771961ee0440985390999a579870
tunsmm <dragon553180@gmail.com>
2022-09-03 18:42:54 +0300

    Realized that I can you 'answer' method instead of 'reply' method if I don't want to reply on user message

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def auth(func):
     "Decorator for checking telegram user id"
     async def wrapper(message):
         if message['from']['id'] != CHAT_ID:
-            return await message.reply("Access Denied", reply=False)
+            return await message.answer("Access Denied")
         return await func(message)
     return wrapper
 
@@ -31,13 +31,12 @@ def auth(func):
 @auth
 async def send_welcome(message: types.Message):
     "Welcome message"
-    await message.reply(
+    await message.answer(
         "Bot for budgeting\n\n"
         "Add expenses: 250 taxi\n"
         "Last expenses: /expenses\n"
         "Categories of expenses: /categories\n"
-        "Statistics: /stats",
-        reply=False)
+        "Statistics: /stats")
 
 
 @dp.message_handler(commands=['expenses'])
@@ -45,7 +44,7 @@ async def send_welcome(message: types.Message):
 async def list_expenses(message: types.Message):
     last_expenses = expenses.last()
     if not last_expenses:
-        await message.reply("No expenses", reply=False)
+        await message.answer("No expenses")
         return
 
     last_expenses_rows = [
@@ -53,7 +52,7 @@ async def list_expenses(message: types.Message):
         f"/del{row['id']} for delete"
         for row in last_expenses]
     answer_message = "Last expenses:\n\n* " + "\n\n* ".join(last_expenses_rows)
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['categories'])
@@ -62,7 +61,7 @@ async def categories_list(message: types.Message):
     categories = Categories().get_all_categories()
     answer_message = "Categories of expenses:\n\n* " +\
             ("\n* ".join([c["name"]+' ('+", ".join(c["aliases"])+')' for c in categories]))
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['stats'])
@@ -76,42 +75,42 @@ async def show_stats(message: types.Message):
                         \n{get_current_year_stats("short")}
                         \nFor more information you can write /help_stats
                       """
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['today'])
 @auth
 async def today_stats(message: types.Message):
     answer_message = get_today_stats("full")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['month'])
 @auth
 async def month_stats(message: types.Message):
     answer_message = get_current_month_stats("full")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['yesterday'])
 @auth
 async def yesterday_stats(message: types.Message):
     answer_message = get_yesterday_stats("full")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['week'])
 @auth
 async def week_stats(message: types.Message):
     answer_message = get_current_week_stats("full")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['year'])
 @auth
 async def year_stats(message: types.Message):
     answer_message = get_current_year_stats("full")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['custom_stats'])
@@ -138,7 +137,7 @@ async def del_expense(message: types.Message):
     row_id = int(message.text[4:])
     expenses.delete_expense(row_id)
     answer_message = "Deleted"
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler()
@@ -147,11 +146,11 @@ async def add_expense(message: types.Message):
     try:
         expense = expenses.add_expense(message.text)
     except exceptions.NotCorrectMessage as e:
-        await message.reply(str(e), reply=False)
+        await message.answer(str(e))
         return
     answer_message = (
         f"Added expenses {expense.amount} on {expense.category_name}.\n\n")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 if __name__ == '__main__':
