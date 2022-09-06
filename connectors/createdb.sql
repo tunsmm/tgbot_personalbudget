4cb1c76ba5237167fe8b7ca0ae79dcaf879af472
tunsmm <dragon553180@gmail.com>
2022-09-07 00:04:10 +0300

    Change strusture of project. You can see in deleted files previous commits

---
+++ connectors/createdb.sql
@@ -0,0 +1,43 @@
+create table budget(
+    codename varchar(255) primary key,
+    daily_limit integer
+);
+
+create table category(
+    codename varchar(255) primary key,
+    name varchar(255),
+    is_base_expense boolean,
+    aliases text
+);
+
+create table expense(
+    id integer primary key,
+    amount integer,
+    created date,
+    category_codename integer,
+    raw_text text,
+    FOREIGN KEY(category_codename) REFERENCES category(codename)
+);
+
+insert into category (codename, name, is_base_expense, aliases)
+values
+    ("products", "продукты", true, "еда, магазин"),
+    ("house", "дом", true, "в дом, house, home, fix price, посуда, хоз, хоз товары, туалет"),
+    ("clothes", "вещи", true, "шмотки, обновки"),
+    ("dinner", "обед", true, "столовая, ланч, бизнес-ланч, бизнес ланч"),
+    ("work", "работа", true, "локтар, тх, work, working"),
+    ("cafe", "кафе", false, "ресторан, кофейня, рест, мак, макдональдс, макдак, kfc, ilpatio, il patio"),
+    ("transport", "общ. транспорт", false, "метро, автобус, metro, subway, маршрутка, bus"),
+    ("taxi", "такси", false, "яндекс такси, yandex taxi, taxi"),
+    ("phone", "телефон", false, "билайн, связь"),
+    ("books", "книги", false, "книга, литература, литра, лит-ра"),
+    ("internet", "интернет", false, "инет, интернет, inet"),
+    ("gifts", "подарки", false, "подарок, present, презентs"),
+    ("subscriptions", "подписки", false, "подписка"),
+    ("other", "прочее", true, "");
+
+insert into budget (codename, daily_limit)
+values
+    ("basic_budget", 1200),
+    ("unbasic_budget", 2000),
+    ("rich_man", 3000);
---
+++ utils/expenses.py
@@ -0,0 +1,47 @@
+from typing import NamedTuple
+
+from utils.categories import Categories
+from utils.messages import parse_message
+from utils.stats import _get_now_formatted
+import connectors.db as db
+
+
+class Expense(NamedTuple):
+    amount: int
+    category_name: str
+
+
+def add_expense(raw_message: str) -> Expense:
+    parsed_message = parse_message(raw_message)
+    category = Categories().get_category(
+        parsed_message.category_text)
+    inserted_row_id = db.insert("expense", {
+        "amount": parsed_message.amount,
+        "created": _get_now_formatted(),
+        "category_codename": category["codename"],
+        "raw_text": raw_message
+    })
+    return Expense(amount=parsed_message.amount,
+                   category_name=category["name"])
+
+
+def last():
+    cursor = db.get_cursor()
+    cursor.execute(
+        "select e.id, e.amount, c.name "
+        "from expense e left join category c "
+        "on c.codename=e.category_codename "
+        "order by created desc limit 10")
+    rows = cursor.fetchall()
+    last_expenses = []
+    for row in rows:
+        last_expenses.append({
+            'amount': row[1],
+            'id': row[0],
+            'category_name': row[2]
+        })
+    return last_expenses
+
+
+def delete_expense(row_id: int) -> None:
+    db.delete("expense", row_id)
