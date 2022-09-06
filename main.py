3b9d15671b20702cf89e5197bbe615f8ec10710a
tunsmm <dragon553180@gmail.com>
2022-09-06 22:12:57 +0300

    Add docs

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from stats import get_current_week_stats, get_current_year_stats, get_today_stat
 import exceptions
 import expenses
 
+# load local environment
 load_dotenv()
 
 API_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -42,6 +43,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(commands=['expenses'])
 @auth
 async def list_expenses(message: types.Message):
+    """Return list of last 10 expenses"""
     last_expenses = expenses.last()
     if not last_expenses:
         await message.answer("No expenses")
@@ -58,6 +60,7 @@ async def list_expenses(message: types.Message):
 @dp.message_handler(commands=['categories'])
 @auth
 async def categories_list(message: types.Message):
+    """Return list of categories"""
     categories = Categories().get_all_categories()
     answer_message = "Categories of expenses:\n\n* " +\
             ("\n* ".join([c["name"]+' ('+", ".join(c["aliases"])+')' for c in categories]))
@@ -67,6 +70,7 @@ async def categories_list(message: types.Message):
 @dp.message_handler(commands=['stats'])
 @auth
 async def show_stats(message: types.Message):
+    """Return basic stats information. Includes stats for today, yesterday, week, month, year"""
     answer_message = f"""
                             {get_today_stats("short")} 
                         \n{get_yesterday_stats("short")} 
@@ -81,6 +85,7 @@ async def show_stats(message: types.Message):
 @dp.message_handler(commands=['today'])
 @auth
 async def today_stats(message: types.Message):
+    """Return stats for today"""
     answer_message = get_today_stats("full")
     await message.answer(answer_message)
 
@@ -88,6 +93,7 @@ async def today_stats(message: types.Message):
 @dp.message_handler(commands=['month'])
 @auth
 async def month_stats(message: types.Message):
+    """Return stats for month"""
     answer_message = get_current_month_stats("full")
     await message.answer(answer_message)
 
@@ -95,6 +101,7 @@ async def month_stats(message: types.Message):
 @dp.message_handler(commands=['yesterday'])
 @auth
 async def yesterday_stats(message: types.Message):
+    """Return stats for yesterday"""
     answer_message = get_yesterday_stats("full")
     await message.answer(answer_message)
 
@@ -102,6 +109,7 @@ async def yesterday_stats(message: types.Message):
 @dp.message_handler(commands=['week'])
 @auth
 async def week_stats(message: types.Message):
+    """Return stats for week"""
     answer_message = get_current_week_stats("full")
     await message.answer(answer_message)
 
@@ -109,6 +117,7 @@ async def week_stats(message: types.Message):
 @dp.message_handler(commands=['year'])
 @auth
 async def year_stats(message: types.Message):
+    """Return stats for year"""
     answer_message = get_current_year_stats("full")
     await message.answer(answer_message)
 
@@ -116,6 +125,12 @@ async def year_stats(message: types.Message):
 @dp.message_handler(commands=['custom_stats'])
 @auth
 async def custom_stats(message: types.Message):
+    """Return custom stats between two periods that user send in the message after the command. 
+    It should be looks like: /custom stats 30012021 15072021
+    where first two numbers of each period are days
+    second two numbers are months
+    last four numbers are years 
+    """
     answer_message = get_custom_stats(message.text)
     await message.answer(answer_message)
 
@@ -123,6 +138,7 @@ async def custom_stats(message: types.Message):
 @dp.message_handler(commands=['help_stats'])
 @auth
 async def help_stats(message: types.Message):
+    """Return help information about stats section"""
     await message.reply(
         "Help stats\n"
         "To see more information about any period just write some of these words: "
@@ -134,7 +150,8 @@ async def help_stats(message: types.Message):
 @dp.message_handler(lambda message: message.text.startswith('/del'))
 @auth
 async def del_expense(message: types.Message):
-    row_id = int(message.text[4:])
+    """Delete certain row of expenses by its id"""
+    row_id = int(message.text[4:]) # text like /del{int}
     expenses.delete_expense(row_id)
     answer_message = "Deleted"
     await message.answer(answer_message)
@@ -143,6 +160,7 @@ async def del_expense(message: types.Message):
 @dp.message_handler()
 @auth
 async def add_expense(message: types.Message):
+    "Add new expense"
     try:
         expense = expenses.add_expense(message.text)
     except exceptions.NotCorrectMessage as e:
